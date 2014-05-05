a0d474ef1532c0f246a2c4b39b118a4ae9828c0c
Emmanuel Amador <manuamador@gmail.com>
2014-05-05 10:51:21 +0200

    Update dipole.py

--- dipole.py
+++ dipole.py
@@ -12,62 +12,9 @@ mu0=4*pi*1e-7
 eps0=1./(mu0*c**2)
 
 
-def Hertz_dipole_ff (r, p, R, phi, f, t=0, epsr=1.):
-  """
-  Calculate E and B field strength of hertzian dipole(s) in the far field
-  p: array of dipole moments [[px0,py0,pz0],[px1,py1,pz1],...[pxn,pyn,pzn]]
-  R: array of dipole positions [[X0,Y0,Z0],[X1,Y1,Z1],...[Xn,Yn,Zn]]
-  r: observation point [x,y,z]
-  f: array of frequencies [f0,f1,...]
-  t: time
-  phi: array with dipole phase angles (0..2pi) [phi0,phi1,...,phin]
-  return: fields values at observation point r at time t for every frequency in f. E and B are (3 components,number of frequencies) arrays.
-  """
-  nf = len(f)
-  rprime = r-R  # r'=r-R
-  if numpy.ndim(p) < 2:
-    magrprime = numpy.sqrt(numpy.sum((rprime)**2))
-    magrprimep = numpy.tile(magrprime, (len(f),1)).T
-    phip = numpy.tile(phi, (len(f),1))
-    w = 2*pi*f  # \omega
-    k = w/c     # wave number
-    krp = k*magrprimep  # k|r'|
-    rprime_cross_p = numpy.cross(rprime, p) # r'x p
-    rp_c_p_c_rp = numpy.cross(rprime_cross_p, rprime) # (r' x p) x r'
-    expfac = numpy.exp(1j*(w*t-krp+phip.T))/(4*pi*eps0*epsr)
-    Ex = (w**2/(c**2*magrprimep**3) * expfac)* (numpy.tile(rp_c_p_c_rp[0],(nf,1))).T
-    Ey = (w**2/(c**2*magrprimep**3) * expfac)* (numpy.tile(rp_c_p_c_rp[1],(nf,1))).T
-    Ez = (w**2/(c**2*magrprimep**3) * expfac)* (numpy.tile(rp_c_p_c_rp[2],(nf,1))).T
-    Bx = expfac/(magrprimep**2*c**3)*(w**2*numpy.tile(rprime_cross_p[0],(nf,1)).T)
-    By = expfac/(magrprimep**2*c**3)*(w**2*numpy.tile(rprime_cross_p[1],(nf,1)).T)
-    Bz = expfac/(magrprimep**2*c**3)*(w**2*numpy.tile(rprime_cross_p[2],(nf,1)).T)
-    E = numpy.vstack((Ex,Ey,Ez))
-    B = numpy.vstack((Bx,By,Bz))
-  else:
-    magrprime = numpy.sqrt(numpy.sum((rprime)**2,axis=1)) # |r'|
-    magrprimep = numpy.tile(magrprime, (len(f),1)).T
-    phip = numpy.tile(phi, (len(f),1))
-    fp = numpy.tile(f,(len(magrprime),1))
-    w = 2*pi*fp  # \omega
-    k = w/c     # wave number
-    krp = k*magrprimep  # k|r'|
-    rprime_cross_p = numpy.cross(rprime, p) # r'x p
-    rp_c_p_c_rp = numpy.cross(rprime_cross_p, rprime) # (r' x p) x r'
-    expfac = numpy.exp(1j*(w*t-krp+phip.T))/(4*pi*eps0*epsr)
-    Ex = (w**2/(c**2*magrprimep**3) * expfac)* (numpy.tile(rp_c_p_c_rp[:,0],(nf,1))).T
-    Ey = (w**2/(c**2*magrprimep**3) * expfac)* (numpy.tile(rp_c_p_c_rp[:,1],(nf,1))).T
-    Ez = (w**2/(c**2*magrprimep**3) * expfac)* (numpy.tile(rp_c_p_c_rp[:,2],(nf,1))).T
-    Bx = expfac/(magrprimep**2*c**3)*(w**2*numpy.tile(rprime_cross_p[:,0],(nf,1)).T)
-    By = expfac/(magrprimep**2*c**3)*(w**2*numpy.tile(rprime_cross_p[:,1],(nf,1)).T)
-    Bz = expfac/(magrprimep**2*c**3)*(w**2*numpy.tile(rprime_cross_p[:,2],(nf,1)).T)
-    E = numpy.vstack((numpy.sum(Ex,axis=0),numpy.sum(Ey,axis=0),numpy.sum(Ez,axis=0)))
-    B = numpy.vstack((numpy.sum(Bx,axis=0),numpy.sum(By,axis=0),numpy.sum(Bz,axis=0)))
-  return E,B
-
-
 def Hertz_dipole (r, p, R, phi, f, t=0, epsr=1.):
   """
-  Calculate E and B field strength of hertzian dipole(s)
+  Calculate E and B field strength radiated by hertzian dipole(s).
   p: array of dipole moments [[px0,py0,pz0],[px1,py1,pz1],...[pxn,pyn,pzn]]
   R: array of dipole positions [[X0,Y0,Z0],[X1,Y1,Z1],...[Xn,Yn,Zn]]
   r: observation point [x,y,z]
@@ -119,10 +66,63 @@ def Hertz_dipole (r, p, R, phi, f, t=0, epsr=1.):
     B = numpy.vstack((numpy.sum(Bx,axis=0),numpy.sum(By,axis=0),numpy.sum(Bz,axis=0)))
   return E,B
 
+def Hertz_dipole_ff (r, p, R, phi, f, t=0, epsr=1.):
+  """
+  Calculate E and B field strength radaited by hertzian dipole(s) in the far field.
+  p: array of dipole moments [[px0,py0,pz0],[px1,py1,pz1],...[pxn,pyn,pzn]]
+  R: array of dipole positions [[X0,Y0,Z0],[X1,Y1,Z1],...[Xn,Yn,Zn]]
+  r: observation point [x,y,z]
+  f: array of frequencies [f0,f1,...]
+  t: time
+  phi: array with dipole phase angles (0..2pi) [phi0,phi1,...,phin]
+  return: fields values at observation point r at time t for every frequency in f. E and B are (3 components,number of frequencies) arrays.
+  """
+  nf = len(f)
+  rprime = r-R  # r'=r-R
+  if numpy.ndim(p) < 2:
+    magrprime = numpy.sqrt(numpy.sum((rprime)**2))
+    magrprimep = numpy.tile(magrprime, (len(f),1)).T
+    phip = numpy.tile(phi, (len(f),1))
+    w = 2*pi*f  # \omega
+    k = w/c     # wave number
+    krp = k*magrprimep  # k|r'|
+    rprime_cross_p = numpy.cross(rprime, p) # r'x p
+    rp_c_p_c_rp = numpy.cross(rprime_cross_p, rprime) # (r' x p) x r'
+    expfac = numpy.exp(1j*(w*t-krp+phip.T))/(4*pi*eps0*epsr)
+    Ex = (w**2/(c**2*magrprimep**3) * expfac)* (numpy.tile(rp_c_p_c_rp[0],(nf,1))).T
+    Ey = (w**2/(c**2*magrprimep**3) * expfac)* (numpy.tile(rp_c_p_c_rp[1],(nf,1))).T
+    Ez = (w**2/(c**2*magrprimep**3) * expfac)* (numpy.tile(rp_c_p_c_rp[2],(nf,1))).T
+    Bx = expfac/(magrprimep**2*c**3)*(w**2*numpy.tile(rprime_cross_p[0],(nf,1)).T)
+    By = expfac/(magrprimep**2*c**3)*(w**2*numpy.tile(rprime_cross_p[1],(nf,1)).T)
+    Bz = expfac/(magrprimep**2*c**3)*(w**2*numpy.tile(rprime_cross_p[2],(nf,1)).T)
+    E = numpy.vstack((Ex,Ey,Ez))
+    B = numpy.vstack((Bx,By,Bz))
+  else:
+    magrprime = numpy.sqrt(numpy.sum((rprime)**2,axis=1)) # |r'|
+    magrprimep = numpy.tile(magrprime, (len(f),1)).T
+    phip = numpy.tile(phi, (len(f),1))
+    fp = numpy.tile(f,(len(magrprime),1))
+    w = 2*pi*fp  # \omega
+    k = w/c     # wave number
+    krp = k*magrprimep  # k|r'|
+    rprime_cross_p = numpy.cross(rprime, p) # r'x p
+    rp_c_p_c_rp = numpy.cross(rprime_cross_p, rprime) # (r' x p) x r'
+    expfac = numpy.exp(1j*(w*t-krp+phip.T))/(4*pi*eps0*epsr)
+    Ex = (w**2/(c**2*magrprimep**3) * expfac)* (numpy.tile(rp_c_p_c_rp[:,0],(nf,1))).T
+    Ey = (w**2/(c**2*magrprimep**3) * expfac)* (numpy.tile(rp_c_p_c_rp[:,1],(nf,1))).T
+    Ez = (w**2/(c**2*magrprimep**3) * expfac)* (numpy.tile(rp_c_p_c_rp[:,2],(nf,1))).T
+    Bx = expfac/(magrprimep**2*c**3)*(w**2*numpy.tile(rprime_cross_p[:,0],(nf,1)).T)
+    By = expfac/(magrprimep**2*c**3)*(w**2*numpy.tile(rprime_cross_p[:,1],(nf,1)).T)
+    Bz = expfac/(magrprimep**2*c**3)*(w**2*numpy.tile(rprime_cross_p[:,2],(nf,1)).T)
+    E = numpy.vstack((numpy.sum(Ex,axis=0),numpy.sum(Ey,axis=0),numpy.sum(Ez,axis=0)))
+    B = numpy.vstack((numpy.sum(Bx,axis=0),numpy.sum(By,axis=0),numpy.sum(Bz,axis=0)))
+  return E,B
+
+
 
 def Hertz_dipole_nf (r, p, R, phi, f, t=0, epsr=1.):
   """
-  Calculate E and B field strength of hertzian dipole(s)
+  Calculate E and B field strength radiated by hertzian dipole(s)  in the near field.
   p: array of dipole moments [[px0,py0,pz0],[px1,py1,pz1],...[pxn,pyn,pzn]]
   R: array of dipole positions [[X0,Y0,Z0],[X1,Y1,Z1],...[Xn,Yn,Zn]]
   r: observation point [x,y,z]
